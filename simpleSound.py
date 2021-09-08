f5e7386b77311e69ed6c8d62f6d2b6e7d5391fbf
nekumelon <89540931+nekumelon@users.noreply.github.com>
2021-09-08 07:46:38 -0700

    Update simpleSound.py

--- simpleSound.py
+++ simpleSound.py
@@ -1,1 +1,14 @@
+import os, ctypes
 
+def windowsCommand(command):
+    buffer = ctypes.create_unicode_buffer(600);
+    ctypes.windll.winmm.mciSendStringW(command, buffer, 559, 0);
+
+class SimpleSound():
+    def play(self, fileName):
+        if (os.path.isfile(fileName)):
+            windowsCommand("open " + fileName);
+            windowsCommand("play " + fileName + " wait");
+            windowsCommand("close " + fileName);
+        else:
+            print(fileName + " could not be found.");
