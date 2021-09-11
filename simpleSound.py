1b2b45632a935cebc5024fd30f833194b56ef0bf
nekumelon <89540931+nekumelon@users.noreply.github.com>
2021-09-10 23:09:52 -0700

    cleanup

--- simpleSound.py
+++ simpleSound.py
@@ -1,8 +1,7 @@
 import ctypes
 
 def windowsCommand(command):
-    buffer = ctypes.create_unicode_buffer(600);
-    ctypes.windll.winmm.mciSendStringW(command, buffer, 559, 0);
+    ctypes.windll.winmm.mciSendStringW(command, ctypes.create_unicode_buffer(600), 559, 0);
 
 def play(fileName):
     windowsCommand("open " + fileName);
