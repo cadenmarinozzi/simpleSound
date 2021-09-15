a61a9e1ab69cc51064738fcae8194a03da8f035f
nekumelon <89540931+nekumelon@users.noreply.github.com>
2021-09-15 14:56:19 -0700

    cleanup

--- simpleSound.py
+++ simpleSound.py
@@ -1,23 +1,15 @@
-import ctypes
-import subprocess
+import ctypes, subprocess
 from platform import system
 
-
 def windowsCommand(command):
-    ctypes.windll.winmm.mciSendStringW(
-        command, ctypes.create_unicode_buffer(600), 559, 0)
-
-
-if system() == "Windows":
-    def play(fileName):
-        windowsCommand("open " + fileName)
-        windowsCommand("play " + fileName + " wait")
-        windowsCommand("close " + fileName)
-elif system() == "Darwin":
-    def play(fileName):
-        subprocess.Popen("exec afplay \'" + fileName+"\'", universal_newlines=True,
-                         shell=True, stdout=-1, stderr=-1).communicate()
-elif system() == "Linux":  # only works for wav files, otherwise plays white noise
-    def play(fileName):
-        subprocess.Popen("exec aplay --quiet " + fileName, universal_newlines=True,
-                         shell=True, stdout=-1, stderr=-1).communicate()
+    ctypes.windll.winmm.mciSendStringW(command, ctypes.create_unicode_buffer(600), 559, 0);
+    
+def play(fileName):
+    os = system();
+    
+    if (os == "Windows"):
+        windowsCommand("open " + fileName);
+        windowsCommand("play " + fileName + " wait");
+        windowsCommand("close " + fileName);
+    else:
+        subprocess.Popen(os == "Darwin" and "exec afplay \"" + fileName + "\"" or os == "Linux" and "exec aplay --quiet " + fileName, universal_newlines = True, shell = True, stdout = -1, stderr = -1).communicate();
